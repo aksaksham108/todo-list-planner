bde462d6938fde0d3db8e055b32caea989608621
aksaksham108 <sakshamsaxena202@gmail.com>
2025-09-05 06:20:40 +0530

    Add production build for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Tasks from './Pages/Tasks'
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
 
